9db2ee051f887ea60aa10ade9316ac63f08a61b3
Tom <tom.fabre31@gmail.com>
2024-11-19 16:18:04 +0100

    simulation.main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,69 @@
+// Généré avec ChatGPT pour l'exemple
+
+import java.util.Scanner;
+
+import Player.Ressource.Ressource;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Création de différentes ressources pour le joueur
+        Ressource mana = new Ressource("Mana", 100);
+        Ressource rage = new Ressource("Rage", 50);
+
+        // Affichage de l'état initial des ressources
+        System.out.println("Bienvenue dans le jeu RPG !");
+        System.out.println("Voici l'état initial de vos ressources :");
+        System.out.println(mana);
+        System.out.println(rage);
+
+        // Simulation d'interactions avec le joueur
+        Scanner scanner = new Scanner(System.in);
+        boolean jeuEnCours = true;
+
+        while (jeuEnCours) {
+            System.out.println("\nQue souhaitez-vous faire ?");
+            System.out.println("1. Consommer du mana");
+            System.out.println("2. Régénérer du mana");
+            System.out.println("3. Consommer de la rage");
+            System.out.println("4. Régénérer de la rage");
+            System.out.println("5. Quitter le jeu");
+
+            int choix = scanner.nextInt();
+
+            switch (choix) {
+                case 1:
+                    System.out.print("Combien de mana souhaitez-vous consommer ? ");
+                    int quantiteManaConsommee = scanner.nextInt();
+                    mana.consommer(quantiteManaConsommee);
+                    System.out.println("Mana après consommation : " + mana);
+                    break;
+                case 2:
+                    System.out.print("Combien de mana souhaitez-vous régénérer ? ");
+                    int quantiteManaRegenerée = scanner.nextInt();
+                    mana.regenerer(quantiteManaRegenerée);
+                    System.out.println("Mana après régénération : " + mana);
+                    break;
+                case 3:
+                    System.out.print("Combien de rage souhaitez-vous consommer ? ");
+                    int quantiteRageConsommee = scanner.nextInt();
+                    rage.consommer(quantiteRageConsommee);
+                    System.out.println("Rage après consommation : " + rage);
+                    break;
+                case 4:
+                    System.out.print("Combien de rage souhaitez-vous régénérer ? ");
+                    int quantiteRageRegenerée = scanner.nextInt();
+                    rage.regenerer(quantiteRageRegenerée);
+                    System.out.println("Rage après régénération : " + rage);
+                    break;
+                case 5:
+                    System.out.println("Merci d'avoir joué !");
+                    jeuEnCours = false;
+                    break;
+                default:
+                    System.out.println("Choix invalide. Veuillez réessayer.");
+            }
+        }
+
+        scanner.close();
     }
 }
--- src/Player/Player.java
+++ src/Player/Player.java
@@ -1,51 +1,66 @@
+// Package Player, contenant la classe définissant les attributs d'un joueur
 package Player;
 
+// Définition de la classe Player, représentant un joueur dans le jeu
 public class Player {
-    private String Nom;
-    private String Prenom;
-    private int vie;
-    private int niveau;
+    // Attributs privés pour stocker les informations du joueur
+    private String Nom;       // Le nom de famille du joueur
+    private String Prenom;    // Le prénom du joueur
+    private int vie;          // La vie actuelle du joueur (points de vie)
+    private int niveau;       // Le niveau du joueur dans le jeu
 
+    // Constructeur par défaut, nécessaire si on souhaite créer un joueur sans initialiser les attributs tout de suite
     public Player() {}
+
+    // Constructeur avec paramètres pour initialiser un joueur avec des valeurs spécifiques
     public Player(String nom, String prenom, int vie, int niveau) {
-        Nom = nom;
-        Prenom = prenom;
-        this.vie = vie;
-        this.niveau = niveau;
+        Nom = nom;           // Initialisation du nom de famille
+        Prenom = prenom;     // Initialisation du prénom
+        this.vie = vie;      // Initialisation de la vie
+        this.niveau = niveau; // Initialisation du niveau
     }
 
+    // Getter pour obtenir le nom du joueur
     public String getNom() {
         return Nom;
     }
 
+    // Setter pour modifier le nom du joueur
     public void setNom(String nom) {
         Nom = nom;
     }
 
+    // Getter pour obtenir le prénom du joueur
     public String getPrenom() {
         return Prenom;
     }
 
+    // Setter pour modifier le prénom du joueur
     public void setPrenom(String prenom) {
         Prenom = prenom;
     }
 
+    // Getter pour obtenir la vie actuelle du joueur
     public int getVie() {
         return vie;
     }
 
+    // Setter pour modifier la vie du joueur
     public void setVie(int vie) {
         this.vie = vie;
     }
 
+    // Getter pour obtenir le niveau du joueur
     public int getNiveau() {
         return niveau;
     }
 
+    // Setter pour modifier le niveau du joueur
     public void setNiveau(int niveau) {
         this.niveau = niveau;
     }
 
+    // Méthode toString pour afficher les informations du joueur de manière lisible
     @Override
     public String toString() {
         return "Player{" +
